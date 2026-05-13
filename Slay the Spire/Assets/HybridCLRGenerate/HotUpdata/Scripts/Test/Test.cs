51789efd75b632ae26637258a7411a49da1c4c60
yonghumingemail <2339927531@qq.com>
2026-05-13 15:53:57 +0800

    2026年5月13日15:52:39

--- Slay the Spire/Assets/HybridCLRGenerate/HotUpdata/Scripts/Test/Test.cs
+++ Slay the Spire/Assets/HybridCLRGenerate/HotUpdata/Scripts/Test/Test.cs
@@ -13,13 +13,14 @@ public class Test : MonoBehaviour
     public SpriteRenderer render;
  public async void Start()
  { 
-     var spriteatlas = await Addressables.LoadAssetAsync<SpriteAtlas>("Assets/Art/Image/SpriteAtlas/Intent.spriteatlasv2");
+    
     var temp= await AddressablesMgr.Instance.LoadAssetAsync<Sprite>("Assets/Art/Image/SpriteAtlas/Intent.spriteatlasv2[buff1L]"); 
     var temp2= await AddressablesMgr.Instance.LoadAssetAsync<Sprite>("Assets/Art/Image/SpriteAtlas/Intent.spriteatlasv2[debuff1]");
     render.sprite = temp;
     render.sprite = temp;
-    render.sprite = spriteatlas.GetSprite("defend");
+
  }
+ 
 
  private void OnDestroy()
  {
