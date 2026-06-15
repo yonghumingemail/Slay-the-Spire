fdd0747c8bb3b2c08414b0124184c8f132cc1689
yonghumingemail <2339927531@qq.com>
2026-06-15 17:50:12 +0800

    2026年6月15日17:50:09

--- Slay the Spire/Assets/HybridCLRGenerate/HotUpdata/Scripts/Test/Test.cs
+++ Slay the Spire/Assets/HybridCLRGenerate/HotUpdata/Scripts/Test/Test.cs
@@ -1,17 +1,26 @@
 using System;
+using Cysharp.Threading.Tasks;
+using UnityEditor;
 using UnityEngine;
-
+using UnityEngine.AddressableAssets;
+using UnityEngine.SceneManagement;
 
 
 public class Test : MonoBehaviour
 {
-    
+
+    public string text;
+
+    private void Awake()
+    {
+        Debug.Log(1111111111);
+    }
+
     private void Update()
     {
     }
 [ContextMenu("Show")]
-    private void Start22()
+    private async void Start22()
     {
-        print("Assets/GameMain/Scenes/GameStart/GameStart.unity".GetHashCode());
     }
 }
