90f961e7489cecfd28f29bc26106d3450fb23857
yonghumingemail <2339927531@qq.com>
2026-06-24 08:36:45 +0800

    2026年6月24日08:36:41

--- Slay the Spire/Assets/HybridCLRGenerate/HotUpdata/Scripts/UI/Menu/MainMenu.cs
+++ Slay the Spire/Assets/HybridCLRGenerate/HotUpdata/Scripts/UI/Menu/MainMenu.cs
@@ -1,6 +1,52 @@
+using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    
+    private GameObject buttonPrefab;
+    [SerializeField] private GameObject menuPanel;
+
+    public Button startGameButton;
+    public Button exitGameButton;
+    public Button settingsButton;
+
+    private void Awake()
+    {
+        Init().Forget();
+    }
+
+    private async UniTask Init()
+    {
+        buttonPrefab =
+            await AddressablesMgr.Instance.LoadAssetAsync<GameObject>("Assets/Art/Prefab/UI/Component/Button.prefab");
+        startGameButton = Instantiate(buttonPrefab, menuPanel.transform).GetComponent<Button>();
+        exitGameButton = Instantiate(buttonPrefab, menuPanel.transform).GetComponent<Button>();
+        settingsButton = Instantiate(buttonPrefab, menuPanel.transform).GetComponent<Button>();
+        
+        startGameButton.onClick.AddListener(OnStartGameButtonClick);
+        exitGameButton.onClick.AddListener(OnExitGameButtonClick);
+        settingsButton.onClick.AddListener(OnSettingsButtonClick);
+        
+    }
+
+    private void OnStartGameButtonClick()
+    {
+        
+    }
+
+    private void OnExitGameButtonClick()
+    {
+        
+    }
+
+    private void OnSettingsButtonClick()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        AddressablesMgr.Instance.Release("Assets/Art/Prefab/UI/Component/Button.prefab");
+    }
 }
